c460cb1afc6abefcf3158528aed3c114e594a40c
Olexiy Buyanskyy <tbot.olexiyb@gmail.com>
2020-11-17 16:07:47 +0200

    the solution based on low C# std functionality

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace cynet_assignment
+{
+    /**
+     * 
+     * Next examples from documentation were used to develop the code
+     * https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.tcplistener?view=net-5.0
+     * https://docs.microsoft.com/ru-ru/dotnet/api/system.io.filestream.lock?view=net-5.0
+     */
+    class CynetListener
+    {
+        static void Main()
+        {
+            TcpListener server = null;
+            try
+            {
+                Int32 port = 7777;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+                String tmpDir = Path.GetTempPath();
+                if (!Directory.Exists(tmpDir))
+                {
+                    Directory.CreateDirectory(tmpDir);
+                }
+                Environment.CurrentDirectory = (tmpDir);
+                String trafficFile = "traffic.txt";
+                Console.WriteLine("Listen on http://{0}:{1} traffic output={2}/{3}", localAddr, port, tmpDir, trafficFile);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // We open or create a file stream for traffic data with exclusive rights
+                using (FileStream fileStream = new FileStream(
+                    trafficFile, FileMode.OpenOrCreate,
+                    FileAccess.Write, FileShare.None))
+                {
+                    fileStream.Seek(0, SeekOrigin.End);
+
+                    ProcessTraffic(server, fileStream);
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+
+        private static void ProcessTraffic(TcpListener server, FileStream fileStream)
+        {
+            // Buffer for reading data, we use the same as
+            // https://referencesource.microsoft.com/#mscorlib/system/io/filestream.cs,396
+            Byte[] bytes = new Byte[4096];
+            System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+
+            // Enter the listening loop.
+            while (true)
+            {
+                // Perform a blocking call to accept requests.
+                // You could also use server.AcceptSocket() here.
+                TcpClient client = server.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+
+                int i;
+                // Loop to receive all the data sent by the client.
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    fileStream.Write(bytes, 0, i);
+                }
+
+                fileStream.Write(encoding.GetBytes(Environment.NewLine));
+                fileStream.Flush();
+
+                // Shutdown and end connection
+                client.Close();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # cynet-assignment
+
+To complete the [Assignment]( https://onedrive.live.com/view.aspx?cid=3028dbdcc7b0dadf&page=view&resid=3028DBDCC7B0DADF!489616&parId=3028DBDCC7B0DADF!489615&authkey=!AO3hW2VXQm8NB5k&app=Word)
+I have had to research on next topics
+
+## Tcp listeners
+
+[StackOverflow discussion](https://stackoverflow.com/questions/47525939/multi-threaded-tcp-listner-server-in-c-sharp) pointed me to several candidates
+
+ * [DotNetty used on Azure IoT services](https://github.com/Azure/DotNetty/tree/dev/examples/WebSockets.Server)
+ * [System.IO.Pipelines](https://devblogs.microsoft.com/dotnet/system-io-pipelines-high-performance-io-in-net/).
+ * [Akka.Streams TCP stream](https://getakka.net/articles/streams/workingwithstreamingio.html).
+ * [TcpListener](https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.tcplistener?view=net-5.0)
+ 
+ 
+ I have picked the simplest sync implementation using TcpListener due to requirement to store traffic into single file.
+
+## File output
+
+ * [FileStream](https://docs.microsoft.com/ru-ru/dotnet/api/system.io.filestream?view=net-5.0)
+ * [StreamWriter](https://docs.microsoft.com/ru-ru/dotnet/api/system.io.streamwriter?view=net-5.0)
+
+I have picked `FileStream` to use file locking `FileShare.None` to block write to this file from other applications, besides `StreamWriter` is actually based on FileStream
+
+Sync implementation usually is faster as we need to lock/unlock the output file in async implementation and File.IO
+operations are very expensive and it should be cheaper to open exclusively `traffic.txt` and output traffic using sync Write() method.
+ 
+From my experience to satisfy
+
+`Make Sure that we can be persistent.`
+`Create a smart processing operation that will lower the chances of having bottlenecks.`
+
+I would ask to change requirement to use a file to keep the traffic information
+The most bullet proof implementation would be [scylla database](https://github.com/datastax/csharp-driver/), 
+that has replication mechanism and load balancing.
+
+Another solution would be use of [zeromq](https://github.com/zeromq/netmq)
+
+Here is very good article on how to implement reliable client/server solution for simple [Request-Reply](https://zguide.zeromq.org/docs/chapter4/) patterns with code examples.
+ 
+ 
+ 
+
